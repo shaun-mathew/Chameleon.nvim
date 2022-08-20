27d35fca229b8d41c4dffdb79ec3e2b231fe5176
shaun.mathew <shaunmathew123@gmail.com>
2022-08-20 13:43:28 -0400

    Converted to plugin

---
+++ lua/chameleon/init.lua
@@ -0,0 +1,79 @@
+local M = {}
+local fn = vim.fn
+local api = vim.api
+M.original_color = nil
+
+local get_kitty_background = function()
+	if M.original_color == nil then
+		fn.jobstart({ "kitty", "@", "get-colors" }, {
+			on_stdout = function(_, d, _)
+				for _, result in ipairs(d) do
+					if string.match(result, "^background") then
+						local color = vim.split(result, "%s+")[2]
+						M.original_color = color
+						break
+					end
+				end
+			end,
+		})
+	end
+end
+
+local change_background = function(color, sync)
+	local arg = 'background="' .. color .. '"'
+	local command = "kitty @ set-colors " .. arg
+	if not sync then
+		fn.jobstart(command, {
+		})
+	else
+		fn.system(command)
+	end
+end
+
+local function get_color(group, attr)
+	return fn.synIDattr(fn.synIDtrans(fn.hlID(group)), attr)
+end
+
+local setup_autocmds = function()
+	local autocmd = api.nvim_create_autocmd
+	local autogroup = api.nvim_create_augroup
+	local bg_change = autogroup("BackgroundChange", { clear = true })
+
+	autocmd("ColorScheme", {
+		pattern = "*",
+		callback = function()
+			local color = get_color("Normal", "bg")
+			change_background(color)
+		end,
+		group = bg_change,
+	})
+
+	autocmd("User", {
+		pattern = "NvChadThemeReload",
+		callback = function()
+			local color = get_color("Normal", "bg")
+			if color then
+				change_background(color)
+			end
+		end,
+		group = bg_change,
+	})
+
+	autocmd("VimLeavePre", {
+		callback = function()
+			if M.original_color ~= nil then
+				change_background(M.original_color, true)
+			end
+		end,
+		group = autogroup("BackgroundRestore", { clear = true }),
+	})
+end
+
+M.setup = function()
+	get_kitty_background()
+	setup_autocmds()
+	local color = get_color("Normal", "bg")
+	change_background(color, true)
+end
+
+return M
